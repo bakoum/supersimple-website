0c0b0f09cfcbf08ff68962dc7aba3d3d4ceb37da
bakoum <56392725+bakoum@users.noreply.github.com>
2025-01-07 21:47:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ function showCurrentTime() {
     const currentTime = new Date().toLocaleTimeString();
     document.getElementById("message").textContent = `Current time is: ${currentTime}`;
 }
+ 
+        function addNumbers() {
+            const num1 = parseFloat(document.getElementById('num1').value);
+            const num2 = parseFloat(document.getElementById('num2').value);
+            const sum = num1 + num2;
+            document.getElementById('result').textContent = isNaN(sum) ? 'Invalid Input' : sum;
+        }
+
+        function subtractNumbers() {
+            const num1 = parseFloat(document.getElementById('num1').value);
+            const num2 = parseFloat(document.getElementById('num2').value);
+            const difference = num1 - num2;
+            document.getElementById('result').textContent = isNaN(difference) ? 'Invalid Input' : difference;
+        }
+
+        function multiplyNumbers() {
+            const num1 = parseFloat(document.getElementById('num1').value);
+            const num2 = parseFloat(document.getElementById('num2').value);
+            const product = num1 * num2;
+            document.getElementById('result').textContent = isNaN(product) ? 'Invalid Input' : product;
+        }
+        
+        function divideNumbers() {
+            const num1 = parseFloat(document.getElementById('num1').value);
+            const num2 = parseFloat(document.getElementById('num2').value);
+            
+           
+            
+            const product = num1 / num2;            
+            const remainder = num1 % num2;
+            const quotient = (num1 - (num1 % num2)) / num2;
+            
+            
+            document.getElementById('result').textContent = isNaN(product) ? 'Invalid Input' : product;
+            document.getElementById('remainder').textContent = isNaN(remainder) ? 'Invalid Input' : remainder;
+            document.getElementById('quotient').textContent = isNaN(quotient) ? 'Invalid Input' : quotient;
+        }
+    
