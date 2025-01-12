db5f9758ea73dd6d3b1d825b06002a66581f1e14
bakoum <56392725+bakoum@users.noreply.github.com>
2025-01-12 11:25:21 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ function showCurrentTime() {
     const currentTime = new Date().toLocaleTimeString();
     document.getElementById("message").textContent = `Current time is: ${currentTime}`;
 }
- 
-        function addNumbers() {
+
+ function addNumbers() {
             const num1 = parseFloat(document.getElementById('num1').value);
             const num2 = parseFloat(document.getElementById('num2').value);
             const sum = num1 + num2;
@@ -48,4 +48,3 @@ function showCurrentTime() {
             document.getElementById('remainder').textContent = isNaN(remainder) ? 'Invalid Input' : remainder;
             document.getElementById('quotient').textContent = isNaN(quotient) ? 'Invalid Input' : quotient;
         }
-    
